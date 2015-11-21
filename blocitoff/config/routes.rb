364c5c081e5fa31ab8f9ee15f9905892b008c931
Alexashooo <aleksandar.sunjic@gmail.com>
2015-11-21 21:49:45 +0100

    User almost finished

--- blocitoff/config/routes.rb
+++ blocitoff/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-
-  devise_for :users
+  devise_for :users, controllers: {sessions: "users/sessions", registrations: "registrations"}
   resources :users, only: [:show]
 
 
